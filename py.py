c9ec101f4745258d5dbf1e775f0083d4018f0553
Stem Stem <stem@astanait.edu.kz>
2023-11-10 09:38:02 +0600

    v1.2

--- py.py
+++ py.py
@@ -14,8 +14,48 @@ def prints(cur):
         print(i)
 
 
-cur.execute("SELECT * FROM college")
+def binary(list, find):
+    a = 0
+    b = len(list) - 1
+    while a<b:
+        mid = int((a+b)/2)
+        if list[mid] == find:
+            return mid 
+        elif list[mid] < find:
+            b = mid 
+        elif list[mid] > find:
+            a = mid 
+    return False
+
+
+
+cur.execute("drop table college")
+cur.execute("create table if not exists college(id_ serial primary key, name text);")
+cur.execute("insert into college(name) values ('john'), ('lube'), ('alzhan')")
+
+
+
+cur.execute("SELECT * FROM college order by id_ asc")
+namb = [r[0] for r in cur.fetchall()]
+cur.execute("SELECT * FROM college order by name asc")
 namee = [r[1] for r in cur.fetchall()]
+
+
+print(namb)
+
+
+
+n = int(input('число: '))
+print(binary(namb, n))
+# print(namee[binary(namb, n)])
+
+
+
+
+
+
+
+
 while True:
     name = input("введите ваше имя: ")
     if name in namee:
